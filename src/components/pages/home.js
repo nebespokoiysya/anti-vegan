6a5633f8a9f49cc29f15113befe3391fbc8e048d
Your Name <you@example.com>
2023-02-16 14:29:34 -0500

    Mental Health

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import "../styling/home.css"
-import img_1 from "../images/antivegan.png"
+import img_1 from "../images/Mental.png"
 
 function home() {
   return (
     <div className='home'>
-        <div className="box-left"></div>
+        <div className="box-left">
+          <div className="container">
+         <h1>MentalHealth</h1>
+            <p>This article will represent you about something certanly about your personality mental health<p>
+              <p className='second-p'>WE'LL START WITH COUPLE INTERISTING FACTS AND LEGENDS</p>
+              <p className='third-p'>*first able don't spend lots of time for this article!</p>
+              
+            </p>
+           </p>
+          </div>
+        </div>
         <div className="box-right">
           <img src={img_1} alt="" />
         </div>
